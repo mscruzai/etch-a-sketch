0a7f286e8cd65fcda5449f0be2acfb16e5243c95
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-03-12 16:52:20 +0800

    Done some initial, skeletal codes.

---
+++ jscript.js
@@ -0,0 +1,26 @@
+
+const MAXSIZE = 100;
+
+const canvasDiv = document.querySelector('.canvas');
+
+function createGrid(size){
+
+    let gridRow;
+    let rowCell;
+
+    for(let i=0; i < size; i++){
+        gridRow = document.createElement("div");
+        gridRow.classList.add("row");
+        //gridRow.style.border = "1px solid green";
+        for(let j=0; j < size; j++){
+            rowCell = document.createElement("div");
+            rowCell.classList.add("cell");
+            rowCell.textContent = "A";
+            gridRow.appendChild(rowCell);
+        }
+        canvasDiv.appendChild(gridRow);
+    }
+}
+
+let gridSize = 16; //prompt("Please enter grid size:");
+createGrid(gridSize);
